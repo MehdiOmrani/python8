8133f59a9ff46e2336a258d9d7998f1d61541f08
MehdiOmrani <86834091+MehdiOmrani@users.noreply.github.com>
2021-08-06 10:52:12 +0100

    Add files via upload

---
+++ Untitled2-2.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Question 1
+
+import pandas as pd
+df = pd.DataFrame({'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19]});
+ddf = pd.DataFrame({'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1]});
+dddf = pd.DataFrame({'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']});
+print(df + ddf + dddf)
+
+
+# In[ ]:
+
+
+#Question 2
+import pandas as pd
+import numpy as np
+
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data , index=labels)
+print(df)
+
+
+# In[ ]:
+
+
+#Question 3
+import pandas as pd
+import numpy as np
+
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data , index=labels)
+
+print(df.info())
+
+
+# In[ ]:
+
+
+#Question 4
+import pandas as pd
+import numpy as np
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(exam_data , index=labels)
+print("Original rows:")
+print(df)
+print("\nAppend a new row:")
+df.loc['k'] = [1, 'Suresh', 'yes', 15.5]
+print("Print all records after insert a new record:")
+print(df)
+print("\nDelete the new row and display the original  rows:")
+df = df.drop('k')
+print(df)
+
+
+# In[ ]:
+
+
+#Question 5
+import pandas as pd
+import numpy as np
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(exam_data , index=labels)
+print("Original rows:")
+print(df)
+print("\nDelete the 'attempts' column from the data frame:")
+df.pop('attempts')
+print(df)
+
+
+# In[ ]:
+
+
+#Question 6
+import pandas as pd
+import numpy as np
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+         'sucess' : [1, 0],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+       
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(exam_data , index=labels)
+print("Original rows:")
+print(df)
+print("\nDelete the 'attempts' column from the data frame:")
+df.pop('attempts')
+print(df)
+
+
+# In[ ]:
+
+
+
+
